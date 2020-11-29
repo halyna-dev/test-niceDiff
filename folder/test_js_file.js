90e0cb427ee7429befdb1b551b332328c34ed785
Halyna <g.v.taran@gmail.com>
2020-11-29 15:20:22 +0200

    made some changes in files

--- folder/test_js_file.js
+++ folder/test_js_file.js
@@ -5,13 +5,22 @@ function quickSort(arr) {
     let pivot = arr[0];
     const left = [];
     const right = [];
+
+    const zero = 0;
       
     for (let i = 1; i < arr.length; i++) {
       if (pivot > arr[i]) {
         left.push(arr[i]);
       } else {
-        right.push(arr[i]);
+        right.push(arr[i])
       }
     }
     return quickSort(left).concat(pivot, quickSort(right));
 }
+
+// comment
+
+/*
+* multiline 
+* comment
+*/
